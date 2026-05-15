913c57eef9c192f595703f4856e82f5a904c22d0
Marc <marcventasynegocios@gmail.com>
2026-05-15 18:36:34 -0300

    base

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Code</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <h1>Practicas PHP y MySQL</h1>
+
+    <?php
+        echo "<p>PHP funcionando correctamente</p>";
+    ?>
+
+    <button id="btn">Click</button>
+
+    <script src="js/app.js"></script>
+</body>
+</html>
